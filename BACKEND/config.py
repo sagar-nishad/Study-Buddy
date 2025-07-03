a77e102bfcf1777853a3bc56c04839f308635920
Sagar Nishad <101sagarnishad@gmail.com>
2025-07-03 14:29:08 +0530

    Update config.py

--- BACKEND/config.py
+++ BACKEND/config.py
@@ -1,10 +1,10 @@
 class Config :
     def __init__(self):
         
-        self.geminai_key = "AIzaSyBatXVIjMScyHrF3Qi9sGrMM8N2phXSpWw"
-        self.private_key="private_2zgNk3D9m/oVjY4z2P+fMSbbMWc=",
-        self.public_key="public_lJWP42WQPVnsInvg5VfgOCJA5Jw=",
-        self.url_endpoint="https://ik.imagekit.io/sagarni101",
+        self.geminai_key = ""
+        self.private_key=""
+        self.public_key=""
+        self.url_endpoint=""
 
 
 CFG = Config()
